781ed3925239340e36aa83e66899729f68d7758e
Daveboggi <david.bogdahn@gmx.de>
2024-01-15 12:01:43 +0100

    added updated pipeline and project-plan

--- project/pipeline.py
+++ project/pipeline.py
@@ -4,7 +4,7 @@ import sqlalchemy
 
 
 def getDataFromExcel(url_to_Excel_Data, excel_sheet_name):
-    df = pd.read_excel(url_to_Excel_Data, excel_sheet_name)
+    df = pd.read_excel(url_to_Excel_Data, excel_sheet_name, index_col=None)
     return df
 
 
@@ -16,33 +16,86 @@ def drop_Column(df: pd.DataFrame, constraint):
 
 def pipeline():
     climate_data = 'https://www.umweltbundesamt.de/sites/default/files/medien/361/dokumente/2021_03_10_trendtabellen_thg_nach_sektoren_v1.0.xlsx'
-    cars_with_alternate_drive = 'https://www.kba.de/SharedDocs/Downloads/DE/Statistik/Fahrzeuge/FZ14/fz14_2022.xlsx?__blob=publicationFile&v=4'
+    vehicle_registrations_germany = 'https://www.kba.de/SharedDocs/Downloads/DE/Statistik/Fahrzeuge/FZ14/fz14_2022.xlsx?__blob=publicationFile&v=4'
+    climate_data_muenster = 'https://opendata.stadt-muenster.de/sites/default/files/Muenster-CO2-Emissionen_2021.xls'
+    vehicle_registrations_muenster = 'https://opendata.stadt-muenster.de/sites/default/files/Fahrzeugbestand-Regierungsbezirk-Muenster-2018-2022.xlsx'
 
     #Using Open Datasets to analyze Correlations
     df = pd.read_excel(climate_data, 'CO2')
-    df2 = pd.read_excel(cars_with_alternate_drive, 'FZ 14.2.1')
+    df2 = pd.read_excel(vehicle_registrations_germany, 'FZ 14.2.1')
+    df3 = pd.read_excel(climate_data_muenster, 'CO2 Emissionen (Sektoren)')
+    df4 = pd.read_excel(vehicle_registrations_muenster, 'PKW')
 
-    #Operations for Climate Data
+   
+    #Operations for climate data in germany
 
     #Column operations
     df = drop_Column(df, [f'REF.{i}' for i in range(1, 24)])
     df = drop_Column(df,["REF"])
+    df = df.rename(columns={f'REF.{i}':f'{i-10+2000}' for i in range (24,30)})
+    df = df.rename(columns={'Vorjahr':'2020'})
     df = drop_Column(df,["Unnamed: 0"])
     df = drop_Column(df,[f'Unnamed: {i}' for i in range(34, 37)])
     df = df.dropna(how='all')
+    df = df.rename(columns={'Unnamed: 1': 'Sector'})
+    df = df.rename(columns={'Unnamed: 2': 'Sum'})
     #Row operations
     df = df.drop(index=df.index[0:6])
     df = df.drop(index=df.index[31:])
+    df = df[~df['Sector'].str.startswith('CRF')]
+    print(df)
 
 
-    #Operations for Cars with alternative driving
+    
+    #Operations for vehicle registration data in germany
     df2 = drop_Column(df2, [f'Unnamed: {i}' for i in range(8, 11)])
     df2 = drop_Column(df2, ["Unnamed: 0"])
-    df2 = df2.dropna()
+    #df2 = df2.dropna()
+    df2 = df2.rename(columns={'Unnamed: 1' : 'Year'})
+    df2 = df2.rename(columns={'Unnamed: 2' : 'Benzin'})
+    df2 = df2.rename(columns={'Unnamed: 3' : 'Diesel'})
+    df2 = df2.rename(columns={'Unnamed: 4' : 'Gas (LPG)'})
+    df2 = df2.rename(columns={'Unnamed: 5' : 'Gas (CNG)'})
+    df2 = df2.rename(columns={'Unnamed: 6' : 'Elektro'})
+    df2 = df2.rename(columns={'Unnamed: 7' : 'Hybrid'})
+    df2 = df2.drop(index= df2.index[0:17])
+    df2 = df2.drop(index= df2.index[9:16])
+
+
+
+    #Operations for climate data in muenster
+
+    df3 = df3.drop(index= df3.index[0:5])
+    df3 = df3.rename(columns={'CO2-Emissionen nach Sektoren in (t)': 'Years'})
+    df3 = df3.rename(columns={'Private Haushalte': 'Haushalte'})
+    df3 = df3.rename(columns={'Gewerbe + Sonstiges': 'Gewerbe'})
+
+
+    #Operations for vehicle registration data in muenster
+
+    df4 = drop_Column(df4,["Regierungsbezirk"])
+    print(df4.columns)
+    df4 = df4.rename(columns={'Statistische Kennziffer und Zulassungsbezirk\n': 'Statistische Kennziffer und Zulassungsbezirk'})
+    df4 = df4.rename(columns={'Nach Kraftstoffarten': 'Benzin'})
+    df4 = df4.rename(columns={'Unnamed: 5': 'Diesel'})
+    df4 = df4.rename(columns={'Unnamed: 6': 'Gas'})
+    df4 = df4.rename(columns={'Unnamed: 7': 'Hybrid'})
+    df4 = drop_Column(df4,["Unnamed: 8"])
+    df4 = df4.rename(columns={'Unnamed: 9': 'Elektro'})
+    df4 = df4.drop(0)
+    idx = df4.columns.get_loc('Unnamed: 10')
+    df4 = df4.iloc[:, :idx]
+    df4['Jahr'] = df4['Jahr'].ffill()
+    df4 = df4[df4['Statistische Kennziffer und Zulassungsbezirk'] == "GESAMT"]
+    
+    
 
     #Writing dfs to new csv
-    df.to_csv('./data/climate_data.csv', sep='\t', encoding='utf-8')
-    df2.to_csv('./data/cars_with_alternative_drive_data.csv', sep='\t', encoding='utf-8')
+    df.to_csv('./data/climate_data_germany.csv', sep='\t', encoding='utf-8')
+    df2.to_csv('./data/vehicle_registrations_germany.csv', sep='\t', encoding='utf-8')
+    df3.to_csv('./data/climate_data_muenster.csv', sep='\t', encoding='utf-8')
+    df4.to_csv('./data/vehicle_registrations_muenster.csv', sep='\t', encoding='utf-8')
+
 
 
 pipeline()
--- project/project-plan.md
+++ project/project-plan.md
@@ -2,7 +2,7 @@
 
 ## Title
 <!-- Give your project a short title. -->
-Analyzing the impact of electric vehicles to the co2 value and the air quality in germany
+Analyzing the impact of electric vehicles to the co2 value in germany
 
 ## Main Question
 
@@ -12,23 +12,38 @@ Do electric vehicles have a significant impact on the co2 emission value in germ
 ## Description
 
 <!-- Describe your data science project in max. 200 words. Consider writing about why and how you attempt it. -->
-This project faces the topic emobility in sense of analyzing the impact of electric vehicles on the CO2 emission in germany. The CO2 emission value will be analyzed from 2014 until 2020 in germany and from 2018 to 2022 in Münster. Compared to that the project analyzes the registration of new vehicles for different driving opportunities (e.g. electric, hybrid, petrol, ...). 
+This project faces the topic e-mobility in sense of analyzing the impact of electric vehicles on the CO2 emission in germany. To have a better analysis we compare it to the vehicle and co2 data of Münster, to see if our result is comparable. This analysis can be used to decide wether electric vehicles should be more supported or are hyped to much.
 
 ## Datasources
 
 <!-- Describe each datasources you plan to use in a section. Use the prefic "DatasourceX" where X is the id of the datasource. -->
 
-### Datasource1: Climate data
+### Datasource1: Climate data of germany
 * Data URL: https://www.umweltbundesamt.de/sites/default/files/medien/361/dokumente/2021_03_10_trendtabellen_thg_nach_sektoren_v1.0.xlsx
 * Data Type: XLSX
 
 * The Dataset containes different air quality values like CO2 or N2 and shows the time related difference over the past years
 
-### Datasource2: 
+### Datasource2: Vehicle registration data of germany
 * Data URL: https://www.kba.de/SharedDocs/Downloads/DE/Statistik/Fahrzeuge/FZ14/fz14_2022.xlsx?__blob=publicationFile&v=4
 * Data Type: XSLX
+* Meta Data: 
 
-* This Dataset shows the number of new admitted cars with alternate drive over the past years
+* The Dataset containes the number of new vehicle registrations over the last decade
+
+### Datasource3: Climate data of muenster
+* Data URL: https://opendata.stadt-muenster.de/sites/default/files/Muenster-CO2-Emissionen_2021.xls
+* Data Type: XSLX
+* Meta Data: https://opendata.stadt-muenster.de/dcatapde/dataset/entwicklung-der-j%C3%A4hrlichen-co2-emissionen-m%C3%BCnster.xml
+
+* The Dataset containes co2 data of the city Münster from 2018 until 2022.
+
+### Datasource4: Vehicle registration data of muenster
+* Data URL: https://opendata.stadt-muenster.de/sites/default/files/Fahrzeugbestand-Regierungsbezirk-Muenster-2018-2022.xlsx
+* Data Type: XSLX
+* Meta Data: https://opendata.stadt-muenster.de/dcatapde/dataset/fahrzeugbestand-regierungsbezirk-m%C3%BCnster-2018-2022.xml
+
+* This Dataset shows the vehicle registrations of the city Münster
 
 ## Work Packages
 
@@ -38,7 +53,7 @@ This project faces the topic emobility in sense of analyzing the impact of elect
 2. Analyzing the co2 emission value over the last 5 years
 3. Analyzing the registration of electric vehicles over the last 5 years
 4. Creating Datapipeline
-5. Compare analysis of data and plot graphs
+5. Compare analysis of data and plot graph
 6. Create Report
 
 
